ece337b1f0fe34db30045ac210599da18201eae1
preeti kumari kushwaha <kushwahapreeti999@gmail.com>
2020-01-23 19:38:22 +0530

    update

--- application/views/Network_test.php
+++ application/views/Network_test.php
@@ -6,8 +6,7 @@
                           <form>
                             <div class="col-sm-12 col-md-12 mb-3">
                           <!-- Radios -->
-                          <strong class="text-muted d-block mb-2">1.A single channel is shared by multiple signals by ____________</strong>
-                          <fieldset>
+                          <strong class="text-muted d-block mb-2">1. A single channel is shared by multiple signals by ____________</strong>
                             <div class="custom-control custom-radio mb-1">
                               <input type="radio" id="formsRadioDefault1" name="formsRadioDefault1" class="custom-control-input">
                               <label class="custom-control-label" for="formsRadioDefault1">analog modulation</label>
@@ -24,27 +23,204 @@
                               <input type="radio" id="formsRadioDisabledChecked4" name="formsRadioDisabledChecked4" class="custom-control-input">
                               <label class="custom-control-label" for="formsRadioDisabledChecked4">phase modulation</label>
                             </div>
-                          </fieldset>
                           <!-- / Radios -->
                         </div>
                             
+
+
                              <strong class="text-muted d-block mb-2">2. In the layer hierarchy as the data packet moves from the upper to the lower layers, headers are ___________</strong>
                           <fieldset>
                             <div class="custom-control custom-radio mb-1">
                               <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
-                              <label class="custom-control-label" for="formsRadioDefault">analog modulation</label>
+                              <label class="custom-control-label" for="formsRadioDefault"> Added</label>
                             </div>
                             <div class="custom-control custom-radio mb-1">
                               <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
-                              <label class="custom-control-label" for="formsRadioChecked">digital modulation</label>
+                              <label class="custom-control-label" for="formsRadioChecked"> Removed</label>
                             </div>
                             <div class="custom-control custom-radio mb-1">
                               <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
-                              <label class="custom-control-label" for="formsRadioDisabled">multiplexing</label>
+                              <label class="custom-control-label" for="formsRadioDisabled"> Rearranged</label>
                             </div>
                             <div class="custom-control custom-radio mb-1">
                               <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
-                              <label class="custom-control-label" for="formsRadioDisabledChecked">phase modulation</label>
+                              <label class="custom-control-label" for="formsRadioDisabledChecked"> Modified</label>
+                            </div>
+                          </fieldset>
+
+
+
+                            <strong class="text-muted d-block mb-2">3. In asynchronous serial communication the physical layer provides ___________</strong>
+                          <fieldset>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDefault">start and stop signallings</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioChecked">flow control</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioDisabled"> both start & stop signalling and flow control</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDisabledChecked"> only start signalling</label>
+                            </div>
+                          </fieldset>
+
+
+                           <strong class="text-muted d-block mb-2">4. Which of the following delay is faced by the packet in travelling from one end system to another?</strong>
+                          <fieldset>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDefault"> Propagation delay</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioChecked"> Queuing delay</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioDisabled">  Transmission delay</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDisabledChecked"> All of the mentioned</label>
+                            </div>
+                          </fieldset>
+
+
+
+                           <strong class="text-muted d-block mb-2">
+5. For a 10Mbps Ethernet link, if the length of the packet is 32bits, the transmission delay is ____________ (in microseconds)</strong>
+                          <fieldset>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDefault"> 3.2</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioChecked">32</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioDisabled"> 0.32</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDisabledChecked">320</label>
+                            </div>
+                          </fieldset>
+
+
+
+                            <strong class="text-muted d-block mb-2">
+6. In the transfer of file between server and client, if the transmission rates along the path is 10Mbps, 20Mbps, 30Mbps, 40Mbps. The throughput is usually ___________</strong>
+                          <fieldset>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDefault">  20Mbps</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioChecked"> 10Mbps</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioDisabled">  40Mbps</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDisabledChecked"> 50Mbps</label>
+                            </div>
+                          </fieldset>
+
+
+                            <strong class="text-muted d-block mb-2">7. Most packet switches use this principle ____________</strong>
+                          <fieldset>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDefault">Stop and wait</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioChecked">Store and forward</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioDisabled"> Both Stop and wait and Store and forward</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDisabledChecked">  Stop and forward</label>
+                            </div>
+                          </fieldset>
+
+
+
+                            <strong class="text-muted d-block mb-2">8. What are the Methods to move data through a network of links and switches?</strong>
+                          <fieldset>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDefault"> Packet switching</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioChecked"> Circuit switching</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioDisabled">  Line switching</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDisabledChecked"> Both Packet switching and Circuit switching</label>
+                            </div>
+                          </fieldset>
+
+
+
+                          <strong class="text-muted d-block mb-2">9. In Go-Back-N window, when the timer of the packet times out, several packets have to be resent even some may have arrived safe. Whereas in Selective Repeat window, the sender resends ___________</strong>
+                          <fieldset>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDefault">Packet which are not lost</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioChecked">Only those packets which are lost or corrupted</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioDisabled"> Packet from starting</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDisabledChecked"> All the packets</label>
+                            </div>
+                          </fieldset>
+
+
+
+                           <strong class="text-muted d-block mb-2">10. Backpressure technique can be applied only to _______</strong>
+                          <fieldset>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDefault" name="formsRadioDefault" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDefault">Congestion networks</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioChecked" name="formsRadioChecked" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioChecked">Closed circuit networks</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabled" name="formsRadioDisabled" class="custom-control-input" >
+                              <label class="custom-control-label" for="formsRadioDisabled"> Open circuit networks</label>
+                            </div>
+                            <div class="custom-control custom-radio mb-1">
+                              <input type="radio" id="formsRadioDisabledChecked" name="formsRadioDisabledChecked" class="custom-control-input">
+                              <label class="custom-control-label" for="formsRadioDisabledChecked">  Virtual circuit networks</label>
                             </div>
                           </fieldset>
                           </form>
