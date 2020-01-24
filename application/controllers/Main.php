5780689554d347f80c36477f20d7d2f9a0db8639
Suraj Uniyal <50100885+surajuniyal@users.noreply.github.com>
2020-01-24 20:25:57 +0530

    1

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -96,7 +96,8 @@ public function Response()
 public function Study_Resourse()
 		{
 				$this->load->view('headermain1');
-				$this->load->view('Forget_password');
+				$this->load->view('MainSidebar_users');
+				$this->load->view('study');
 				$this->load->view('Footermain');		
 		}
 
@@ -131,7 +132,9 @@ public function FeedBack()
 				//$this->load->view('dashboard');
 				$this->load->view('Footermain');		
 		}
+	
 	}
+
 
 
         
