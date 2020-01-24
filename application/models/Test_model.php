b299ae01db2c522c245bb191feabc6502ad34a3c
ravi <ravip8527@gmail.com>
2020-01-25 01:38:10 +0530

    jj

--- application/models/Test_model.php
+++ application/models/Test_model.php
@@ -9,4 +9,11 @@ class Test_model extends CI_Model
 
 		
 		}
+function getQuestion()
+		{
+			$query = $this->db->get("test");
+		return $query->result();
+
+		
+		}
 }
--- application/views/MainSidebar_users.php
+++ application/views/MainSidebar_users.php
@@ -7,15 +7,13 @@
     <div class="container-fluid">
       <div class="row">
         <aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
-          <div class="main-navbar">
+      <div class="main-navbar">
             <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
-              <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
+              <a class="navbar-brand w-100 mr-0" href="<?php echo site_url('main');?>" style="line-height: 25px;">
                 <div class="d-table m-auto">
-                  <a href="<?php echo site_url('main/index');?>">
+                         <img id="main-logo" class="d-inline-block align-top-left mr-1" style="background-color:Black; max-width: 150px;" src="<?php echo base_url('assets/img/logo.png');?>">
           
-                  <img id="main-logo" class="d-inline-block align-top mr-1" style="background-color:Black; width:100%" src="<?php echo base_url('assets/img/logo.png');?>">
-                </a>
-                 </div>
+                   </div>
               </a>
               <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
                 <i class="material-icons">&#xE5C4;</i>
